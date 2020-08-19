57f764cf13bd176fcb5baadbc100ff4315e4dcb5
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-08-19 11:44:40 -0400

    Finished initial process flow. Is now functional.

--- HomeHub.BackgroundServices/SpotifySort/SpotifySort.cs
+++ HomeHub.BackgroundServices/SpotifySort/SpotifySort.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using HomeHub.BackgroundServices.Configuration.SpotifySort;
@@ -65,6 +66,7 @@ namespace HomeHub.BackgroundServices
                     AccessToken = Token.AccessToken
                 };
 
+                // Denotes that we can refresh the token in the future.
                 IsAuthenticated = true;
 
                 semaphore.Release();
@@ -86,27 +88,46 @@ namespace HomeHub.BackgroundServices
             Dictionary<string, string> playlistDescriptions = new Dictionary<string, string>();
 
             // Stores the PlaylistID/SongID relation for the multi call back to the API.
+            Dictionary<string, List<string>> playlistNewSongs = new Dictionary<string, List<string>>();
+            var playlists = GetUserPlaylistsAsync(cancellationToken);
+            var likedSongs = GetUserLikedTracksAsync(cancellationToken);
+            List<Task> tasks = new List<Task>();
 
-            /**
-            * The idea here is to be able to use a multicall to spotify to add all the new songs to the
-            * Playlists, and in the same vein be able to conglomerate a big ol' list for "unlike songs" call.
-            * One of the issues the node version had was that spotify couldn't handle the volume of the
-            * Remove requests, we got duplicates and glitches in unliking songs.
-            * This should let us only iterate through the liked songs once as well.
-            */
-            Dictionary<string, string[]> playlistNewSongs = new Dictionary<string, string[]>();
+            await Task.WhenAll(new Task[] { playlists, likedSongs });
 
-            // TODO -> Add these calls to an Task list and await them all before continuing.
-            var playlists = await GetUserPlaylistsAsync(cancellationToken);
-            var likedSongs = await GetUserLikedTracksAsync(cancellationToken);
+            // TODO -> Move the Get Descriptions and Get Genres to their own things.
+            // TODO -> This could potentially spawn 100+ requests. Consider a semaphore to help less powerful pi?
 
             // Creating the association between the playlist ID and description
-            foreach(var playlist in playlists.Items)
+            foreach(var playlist in playlists.Result.Items)
             {
-                playlistDescriptions[playlist.Id] = await GetPlaylistDescriptionsAsync(playlist.Id, cancellationToken);
+                tasks.Add(Task.Run(async () =>
+                {
+                    playlistDescriptions[playlist.Id] = await GetPlaylistDescriptionsAsync(playlist.Id, cancellationToken);
+                    playlistNewSongs[playlist.Id] = new List<string>();
+                }));
             }
 
-            // Add the songs to the relevant playlists,
+            // Leveraging implicit conversion in this iteration to save on iterations elsewhere.
+            foreach(SavedTrackWithGenre song in likedSongs.Result.Items)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    // Storing as genre track. Paging object isn't super helpful for this.
+                    var songWithGenres = await GetGenreFromSongAsync(song, cancellationToken);
+
+                    // Don't need to save the modified song anywhere IFF we can just directly add it to the dict.
+                    await AddSongsToGenreDictionaryAsync(songWithGenres,
+                                                         playlistDescriptions,
+                                                         playlistNewSongs,
+                                                         cancellationToken);
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+
+            // Multicall for adding to playlists/removing from liked.
+            await MoveNewSongsIntoPlaylistsAsync(playlistNewSongs, cancellationToken);
 
             semaphore.Release();
         }
@@ -119,14 +140,13 @@ namespace HomeHub.BackgroundServices
             string userId = user.Id;
             logger.LogInformation($"Getting playlists from {userId}.");
             Paging<SimplePlaylist> playlists = await api.GetUserPlaylistsAsync(userId, 100);
-
             return playlists;
         }
 
         private async Task<PrivateProfile> GetUserAsync(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            logger.LogInformation($"Requesting user's Private profile.");
+                logger.LogInformation($"Requesting {user?.Id ?? "user"}'s Private profile.");
             PrivateProfile profile = await api.GetPrivateProfileAsync();
             return profile;
         }
@@ -134,18 +154,78 @@ namespace HomeHub.BackgroundServices
         private async Task<Paging<SavedTrack>> GetUserLikedTracksAsync(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            logger.LogInformation($"Getting {user.Id ?? "User"}'s Liked tracks.");
+            logger.LogInformation($"Getting {user?.Id ?? "User"}'s Liked tracks.");
             Paging<SavedTrack> tracks = await api.GetSavedTracksAsync(100);
+            cancellationToken.ThrowIfCancellationRequested();
             return tracks;
         }
 
-        private async Task<string> GetPlaylistDescriptionsAsync(string playlist,
-                                                                CancellationToken cancellationToken)
+        private async Task<string> GetPlaylistDescriptionsAsync(
+            string playlist,
+            CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
             logger.LogInformation($"Getting Playlist description for playlistID: {playlist}");
             FullPlaylist fullPlaylist = await api.GetPlaylistAsync(playlist, "", "");
             return fullPlaylist.Description;
+        }
+
+        private async Task<SavedTrackWithGenre> GetGenreFromSongAsync(
+            SavedTrackWithGenre genreTrack,
+            CancellationToken cancellationToken)
+        {
+            foreach(var artist in genreTrack.Track.Artists)
+            {
+                // Could async this too if performance is really taking a hit, but it probably isn't necessary.
+                logger.LogInformation($"Getting Genres for artist {artist} for song {genreTrack.Track.Name}");
+                var fullArtist = await api.GetArtistAsync(artist.Id);
+                genreTrack.Genres = genreTrack.Genres.Concat(fullArtist.Genres).ToList();
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+            return genreTrack;
+        }
+
+        private async Task AddSongsToGenreDictionaryAsync(
+            SavedTrackWithGenre genreTrack,
+            Dictionary<string, string> genreIdDict,
+            Dictionary<string, List<string>> newSongsDict,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            logger.LogInformation($"Adding {genreTrack.Track.Name} to genreList");
+
+            foreach(string genre in genreTrack.Genres)
+            {
+                foreach(KeyValuePair<string, string> genres in genreIdDict)
+                {
+                    // If the genre exists in any playlist genre, AND isn't already in the newSongsDict.
+                    if (genres.Value.Contains(genre) && (!newSongsDict[genres.Key].Contains(genre)))
+                    {
+                        newSongsDict[genres.Key].Add(genreTrack.Track.Id);
+                    }
+                }
+            }
+        }
+    
+        private async Task MoveNewSongsIntoPlaylistsAsync(
+            Dictionary<string, List<string>> newPlaylistSongsDict,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            List<Task> tasks = new List<Task>();
+            List<string> unlikeList = new List<string>();
+
+            foreach(KeyValuePair<string, List<string>> entry in newPlaylistSongsDict)
+            {
+                logger.LogInformation($"Moving tracks to playlist: {entry.Key}");
+                tasks.Add(api.AddPlaylistTracksAsync(entry.Key, entry.Value));
+                unlikeList = unlikeList.Concat(entry.Value).ToList();
+            }
+
+            await Task.WhenAll(tasks);
+            logger.LogInformation("Unliking moved songs.");
+            await api.RemoveSavedTracksAsync(unlikeList);
         }
     }
 }
