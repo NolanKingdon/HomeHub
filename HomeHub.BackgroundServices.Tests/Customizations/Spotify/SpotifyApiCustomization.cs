743a59d02b7b28c31b392c670a49e535afe3c31f
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-08-22 13:52:50 -0400

    Added unit tests

---
+++ HomeHub.BackgroundServices.Tests/Customizations/Spotify/SpotifyApiCustomization.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture;
+using HomeHub.BackgroundServices.Configuration.SpotifySort;
+using Moq;
+using SpotifyAPI.Web.Models;
+
+namespace HomeHub.BackgroundServices.Tests.Customizations.Spotify
+{
+    public class SpotifyApiCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            // Options already defined in SpotifySortOptionsCustomization.cs.
+            var options = fixture.Create<SpotifySortOptions>();
+            var api = fixture.Freeze<Mock<IApi>>();
+
+            // User Playlists.
+            api.Setup( a => a.GetUserPlaylistsAsync(It.IsAny<string>(), options.MaxPlaylistResults))
+               .ReturnsAsync(() => fixture.Create<Paging<SimplePlaylist>>());
+
+            // User Profile.
+            api.Setup( a => a.GetPrivateProfileAsync())
+               .ReturnsAsync(() => fixture.Create<PrivateProfile>());
+
+            // Liked Songs.
+            api.Setup( a => a.GetSavedTracksAsync(options.MaxSongResults))
+               .ReturnsAsync( () => fixture.Create<Paging<SavedTrack>>());
+
+            // Playlist Descriptions.
+            api.Setup( a => a.GetPlaylistAsync(It.IsAny<string>()))
+               .ReturnsAsync( () => fixture.Create<FullPlaylist>());
+
+            // Artists.
+            api.Setup( a => a.GetArtistAsync(It.IsAny<string>()))
+               .ReturnsAsync( () => fixture.Create<FullArtist>());
+
+            // Move songs - Error Unused.
+            api.Setup( a => a.AddPlaylistTracksAsync(It.IsAny<string>(), It.IsAny<List<string>>()))
+               .ReturnsAsync(() => fixture.Create<ErrorResponse>());
+
+            // Delete Songs - Error Unused.
+            api.Setup( a => a.RemoveSavedTracksAsync(It.IsAny<List<string>>()))
+               .ReturnsAsync(() => fixture.Create<ErrorResponse>());
+        }
+    }
+}
