fded7c1f7f1c2a394d2fb0d0f8d5dd69408175fb
Nolan Kingdon <nolan.kingdon@gmail.com>
2020-12-21 12:12:15 -0500

    Split CommandFile into command interface (Terminal, Command prompt, etc.) and Command args (Actual command)

--- HomeHub.SystemUtils/Configuration/TemperatureOptions.cs
+++ HomeHub.SystemUtils/Configuration/TemperatureOptions.cs
@@ -5,6 +5,7 @@ namespace HomeHub.SystemUtils.Configuration
     public class TemperatureOptions
     {
         public Temperature Unit { get; set; }
-        public string CommandFile { get; set; }
+        public string CommandInterface { get; set; }
+        public string CommandArgs { get; set; }
     }
 }
